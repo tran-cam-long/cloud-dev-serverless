7e8f846e3c1151929a5a8da3f7bafbbbaa54983f
tran-cam-long <camlong.tran0702@gmail.com>
2023-07-10 06:44:37 +0700

    init feature

--- backend/src/businessLogic/todos.ts
+++ backend/src/businessLogic/todos.ts
@@ -1,10 +1,12 @@
 import { TodosAccess } from '../dataLayer/todosAccess'
-import { AttachmentUtils } from '../helpers/attachmentUtils';
+import { AttachmentUtils } from '../helpers/attachmentUtils'
 import { TodoItem } from '../models/TodoItem'
 import { CreateTodoRequest } from '../requests/CreateTodoRequest'
 // import { UpdateTodoRequest } from '../requests/UpdateTodoRequest'
 import { createLogger } from '../utils/logger'
 import * as uuid from 'uuid'
+import { UpdateTodoRequest } from '../requests/UpdateTodoRequest'
+import { TodoUpdate } from '../models/TodoUpdate'
 // import * as createError from 'http-errors'
 
 const logger = createLogger('TodosAccess');
@@ -43,4 +45,20 @@ export async function getAllTodos(userId: string): Promise<TodoItem[]> {
 
   logger.info(`End getAllTodos with userId: ${userId}`);
   return todosList;
+}
+
+// Implement update todos feature
+export async function updateTodo(updateTodo: UpdateTodoRequest, todoId: string, userId: string): Promise<TodoUpdate> {
+  return await todosAccess.updateTodoItem(todoId, userId, updateTodo);
+}
+
+// Implement delete todos feature
+export async function deleteTodo(todoId: string, userId: string): Promise<void>{
+  return await todosAccess.deleteTodoItem(todoId, userId);
+}
+
+// Implement attachment feature
+export async function createAttachmentPresignedUrl(todoId: string, userId: string): Promise<string> {
+  logger.info("Start createAttachmentPresignedUrl with userId: ", userId);
+  return attachmentUtils.getUploadUrl(todoId);
 }
--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -14,7 +14,7 @@ export const handler = middy(
     return {
       statusCode: 200,
       body: JSON.stringify({
-        item: todos
+        items: todos
       })
     };
   }
